9a638a69a03fa2187a1ba1b0aa0bbbdbfa5ca1c0
Srose25 <ros20019@byui.edu>
2025-01-16 20:05:23 -0700

    number guessing game completed // I also made changes to the .json file for my .net type from .net8.0 to .net9.0

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
+using System.Reflection.PortableExecutable;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Creates random number starting with 1 and ending at 10
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        bool machinas = true;
+
+        //A while loop that asks the user for a guess after each attempt
+        while (machinas == true)
+        {
+            //Asking for the user prompt and converting it to an int.
+            //If you guess correctly the loop ends
+            Console.Write("Try to guess the Magic Number: ");
+            string response = Console.ReadLine();
+            int guess = int.Parse(response);
+
+            //Checking to see if the guess is higher, lower, or correct
+            if (guess > number)
+            {
+                Console.WriteLine("Lower!");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher!");
+            }
+
+            else
+            {
+                Console.WriteLine("Congrats!");
+                Console.WriteLine($"The number is {number}");
+                machinas = false;
+            }
+        }
     }
 }
