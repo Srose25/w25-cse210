f27956326345d379d504d7218b0b18f44f7ac14e
Srose25 <ros20019@byui.edu>
2025-01-16 22:02:50 -0700

    completed prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
 class Program
+
 {
+    //Function that welcomes the user to the program
+    //(I'm intentionally leaving in my silly spelling blunder 
+    //to prove that a human being wrote this)
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Hello to the program!");
+    }
+
+    //Function that prompts the user for their name
+    static string PromptUserName()
+    {
+        Console.Write("What's your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    //Function that prompts the user for their favorite number
+    static int PromptUserNum()
+    {
+        Console.Write("What's your favorite number? ");
+        string entry = Console.ReadLine();
+        int userNum = int.Parse(entry);
+        return userNum;
+    }
+
+    //Function that takes their favorite number and squares it
+    static int SquareNum(int userNum)
+    {
+        int sNum = userNum * userNum;
+        return sNum;
+    }
+
+    //Function that displays the results
+    static void DisplayResult(string userName, int sNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {sNum}");
+    }
+
+    //Function that calls all the other functions in sequence
+    //Gathering the data needed to call the later functions as well.
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNum = PromptUserNum();
+        int sNum = SquareNum(userNum);
+        DisplayResult(userName, sNum);
+
     }
 }
