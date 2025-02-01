b046ee1881b626e1301b446da7ddd09f34a92b95
Srose25 <ros20019@byui.edu>
2025-02-01 16:57:14 -0700

    Added one last comment to show that I'm done

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Develop02 World!");
+        //Console.WriteLine("Hello Develop02 World! We did it!");
 
         Menu theMenu = new Menu();
 
