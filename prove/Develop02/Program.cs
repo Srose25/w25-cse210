7d9bae8e80d61c302737e4a04cf85b0bdb9fc077
Srose25 <ros20019@byui.edu>
2025-02-01 17:09:57 -0700

    Added comment for exceed requirement

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,6 +6,10 @@ class Program
     {
         //Console.WriteLine("Hello Develop02 World! We did it!");
 
+        //For the exceed I decided to make an extra class just for the menu
+        //In the Menu class I've added a switch statement to make it organized
+        //This way if I ever add to this program the main function will still be very simple
+
         Menu theMenu = new Menu();
 
         theMenu.DisplayMenu();
