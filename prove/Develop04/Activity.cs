bf1ea7cbf44c19aa2d92fdba8d1ebe3e98f79612
Srose25 <ros20019@byui.edu>
2025-02-28 18:52:17 -0700

    Added more functionality to Parent Class

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -36,17 +36,17 @@ public class Activity
             switch(userInput)
             {
                 case "1":
-                BreatheActivity ba1 = new BreatheActivity("Welcome to the Breathing Activity!", "\nFirst you will choose how long the duration of the activity will be \nand then you will breathe in and out in 5 second increments.\n Try to only focus on that breath during the activity.");
+                BreatheActivity ba1 = new BreatheActivity("Breathing Activity", "\nFirst you will choose how long the duration of the activity will be \nand then you will breathe in and out in 5 second increments.\nTry to only focus on that breath during the activity.");
                 ba1.DoBreathe();
                 break;
 
                 case "2":
-                ReflectActivity ra1 = new ReflectActivity("Welcome to the Reflect Activity", "\nFirst you will choose how long the duration of the activity will be, \nthen you will be given a prompt and as the activity goes on you will reflect on that prompt and on your life.");
+                ReflectActivity ra1 = new ReflectActivity("Reflect Activity", "\nFirst you will choose how long the duration of the activity will be, \nthen you will be given a prompt and as the activity goes on you will reflect on that prompt and on your life.");
                 ra1.DoReflect();
                 break;
 
                 case "3":
-                ListingActivity la1 = new ListingActivity("Welcome to the Listing Activity!", "\nFirst you will choose how long the duration of the activity will be, \nthen you will be given a prompt and as the activity goes on you will list the things you can think of relating to the prompt.");
+                ListingActivity la1 = new ListingActivity("Listing Activity", "\nFirst you will choose how long the duration of the activity will be, \nthen you will be given a prompt and as the activity goes on you will list the things you can think of relating to the prompt.");
                 la1.DoListing();
                 break;
 
@@ -62,6 +62,14 @@ public class Activity
             }
 
         }
+    }
+
+    public void StartMessage(string title, string desc)
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello! Welcome to the {title}!");
+        Thread.Sleep(2000);
+        Console.WriteLine($"\n{desc}");
     }
 
     public int Clock()
@@ -94,5 +102,30 @@ public class Activity
         //create the timer
         time = time * 10 * 1000; //Interval * seconds * miliseconds
         return time;
+    }
+
+    public void AnimateProgressBar(int step)
+    {
+        int barLength = 20;
+        for (int i = 0; i < barLength; i++)
+        {
+            Console.Write("[");
+
+            for (int z = 0; z < barLength; z++ )
+            {
+                if (z > i)
+                {
+                    Console.Write("-");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            Console.Write("]");
+            Thread.Sleep(step);
+
+            Console.Write("\r");
+        }
     }
 }
