0f7b512b08d74f7038af234b4f99765c7e008d02
Srose25 <ros20019@byui.edu>
2025-03-07 22:30:31 -0700

    comment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        //Sorry I was planning on doing this assignment right after I got off work
+        //but my stake was invited to see the Elder Bednar's devotional tonight so 
+        //right now I won't be able to finish this project but I will try to get it submitted next week
+        //I'd love it if I could submit this late and get partial credit but I understand
+        //if it's not part of the policy so let me know what I can do to make it up when you get to this
+        //thank you!
     }
 }
