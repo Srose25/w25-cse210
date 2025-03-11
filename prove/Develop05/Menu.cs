4d9009cecd90861494594ee63db9acf90fdb6e4f
Srose25 <ros20019@byui.edu>
2025-03-10 22:02:16 -0600

    Menu Skeleton

---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,36 @@
+public class Menu()
+{
+    //Attributes
+
+    int _score;
+    List<string> _goalList;
+
+
+    //Behaviors
+
+    public Menu()
+    {
+        _goalList = new List<string>{}; //a new empty list
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void Load()
+    {
+
+    }
+
+    public int UpdateScore() //a method where the score is updated after completing a goal
+    {
+
+        return _score;
+    }
+}
