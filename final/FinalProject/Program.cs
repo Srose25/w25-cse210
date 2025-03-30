31cda2d510bea64c4c2a8433d3aad124d7ca5599
Srose25 <ros20019@byui.edu>
2025-03-29 18:23:29 -0600

    Made it start the program

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -5,33 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello FinalProject World!");
-        Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory());
+        
+        //Debug Sounds
 
+        //Console.WriteLine("Current Directory: " + Directory.GetCurrentDirectory());
+        //MainMenu test1 = new MainMenu();
+        //if (!File.Exists("talk1.wav"))
+        //{
+        //    Console.WriteLine("File not found!");
+        //}
+        //else
+        //{
+        //    bool running = true;
+        //    int go = 0;
+        //    Console.WriteLine("File found!");
+        //    while(running)
+        //    {
+        //        if(go == 10)
+        //        {
+        //            running = false;
+        //        }
+        //        else
+        //        {
+        //            test1.PlaySound("talk1.wav");
+        //            Thread.Sleep(300);
+        //            go += 1;
+        //        }
+        //    }
+        //}
 
-        MainMenu test1 = new MainMenu();
-        if (!File.Exists("talk1.wav"))
-        {
-            Console.WriteLine("File not found!");
-        }
-        else
-        {
-            bool running = true;
-            int go = 0;
-            Console.WriteLine("File found!");
-            while(running)
-            {
-                if(go == 10)
-                {
-                    running = false;
-                }
-                else
-                {
-                    test1.PlaySound("talk1.wav");
-                    Thread.Sleep(300);
-                    go += 1;
-                }
-            }
-        }
+        MainMenu gamestart = new MainMenu();
+        gamestart.Start();
 
     }
 }
